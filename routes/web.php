d67eeeef041d106671ea9edb641c2b3abb2a0734
Andrius Pilvelis <andrius.pilv@gmail.com>
2020-09-09 20:41:18 +0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+
+Route::resource('customers', 'CustomerController');
+Route::resource('country', 'CountryController');
+Route::resource('town', 'TownController');
+Auth::routes();
+
+Route::get('customers/{id}/travel', 'CustomerController@travel')->name('customers.travel');
+Route::get('/home', 'HomeController@index')->name('home');
